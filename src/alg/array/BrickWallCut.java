941a3a3633d89f9344b831ec0787e36eb7196b22
daro <dariusz.baca@gmail.com>
2024-06-23 21:56:06 +0200

    updated

--- src/alg/array/BrickWallCut.java
+++ src/alg/array/BrickWallCut.java
@@ -7,23 +7,24 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 /**
- * A wall consists of several rows of bricks of various integer lengths and uniform height. Your goal is to 
- * find a vertical line going from the top to the bottom of the wall that cuts through the fewest 
+ * A wall consists of several rows of bricks of various integer lengths and uniform height. Your goal is to
+ * find a vertical line going from the top to the bottom of the wall that cuts through the fewest
  * number of bricks. If the line goes through the edge between two bricks, this does not count as a cut.
- * 
+ *
  * Example:
  * [[3, 5, 1, 1],
- *  [2, 3, 3, 2],
- *  [5, 5],
- *  [4, 4, 2],
- *  [1, 3, 3, 3],
- *  [1, 1, 6, 1, 1]]
- *  
- * The best we can we do here is to draw a line after the eighth brick, which will only require cutting 
+ * [2, 3, 3, 2],
+ * [5, 5],
+ * [4, 4, 2],
+ * [1, 3, 3, 3],
+ * [1, 1, 6, 1, 1]]
+ *
+ * The best we can we do here is to draw a line after the eighth brick, which will only require cutting
  * through the bricks in the third and fifth row.
- * 
- * Solution would be to calculate edges of bricks for each row and find element that appears the most - meaning minimal cut.
- * Complexity is O(n) where n is the number of bricks in the wall. 
+ *
+ * Solution would be to calculate edges of bricks for each row and find element that appears the most - meaning minimal
+ * cut.
+ * Complexity is O(n) where n is the number of bricks in the wall.
  */
 public class BrickWallCut {
     public int leastBricks(List<List<Integer>> wall) {
@@ -49,7 +50,7 @@ public class BrickWallCut {
 
     public static void main(String... args) {
         List<List<Integer>> wall = List.of(List.of(3, 5, 1, 1), List.of(2, 3, 3, 2), List.of(5, 5), List.of(4, 4, 2),
-                List.of(1, 3, 3, 3), List.of(1, 1, 6, 1, 1));
+            List.of(1, 3, 3, 3), List.of(1, 1, 6, 1, 1));
         long time = System.nanoTime();
         int minCutEdge = new BrickWallCut().leastBricks(wall);
         time = System.nanoTime() - time;
